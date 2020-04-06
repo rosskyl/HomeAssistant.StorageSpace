3adbd6b214aac38721d5a4a0a70b797492364f01
Kyle <kjr1995@gmail.com>
2020-04-06 18:30:33 -0500

    Get storage and convert to % and publish to MQTT

---
+++ HomeAssistant.StorageSpace/HomeAssistant.StorageSpace/MqttConfig.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeAssistant.StorageSpace
+{
+    class MqttConfig
+    {
+        public string Host { get; set; }
+
+        public string Topic { get; set; }
+    }
+}
--- HomeAssistant.StorageSpace/HomeAssistant.StorageSpace/Program.cs
+++ HomeAssistant.StorageSpace/HomeAssistant.StorageSpace/Program.cs
@@ -1,12 +1,48 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+using Microsoft.Extensions.Configuration.FileExtensions;
+using Microsoft.Extensions.Configuration.Json;
+using uPLibrary.Networking.M2Mqtt;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace HomeAssistant.StorageSpace
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var config = BuildConfig();
+            var mqttConfig = new MqttConfig();
+
+            config.Bind(mqttConfig);
+
+            MqttClient client = new MqttClient(mqttConfig.Host);
+
+            var clientId = Guid.NewGuid().ToString();
+            client.Connect(clientId);
+
+            var drives = DriveInfo.GetDrives();
+            foreach (var drive in drives)
+            {
+                var pct = (double)drive.AvailableFreeSpace / (double)drive.TotalSize * 100;
+                var message = string.Format("{0:0.0}", pct);
+                var topic = mqttConfig.Topic + "/" + drive.Name.Replace(":\\", "");
+                var r = client.Publish(topic, Encoding.UTF8.GetBytes(message));
+                await Task.Delay(1000);
+            }
+
+            client.Disconnect();
+        }
+
+        private static IConfiguration BuildConfig()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            return builder.Build();
         }
     }
 }
