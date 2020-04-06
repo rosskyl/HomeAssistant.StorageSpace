0e762ec41869800ab0eb6dedc4165cd4151c5b96
Kyle <kjr1995@gmail.com>
2020-04-06 17:16:59 -0500

    Initial project commit

---
+++ HomeAssistant.StorageSpace/HomeAssistant.StorageSpace/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HomeAssistant.StorageSpace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
